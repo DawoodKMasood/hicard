a22b2f37d96d74dff278637b507b6841ef44c700
Dawood Khan Masood <91176669+DawoodKMasood@users.noreply.github.com>
2022-12-05 00:11:06 +0500

    basic structure

--- src/lib/components/Card.js
+++ src/lib/components/Card.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+// import style from Utilities.css
+import './style/Utilities.css';
+
+// import style from Card.css
+import './style/Card.css';
+
 const Card = (props) => {
+    const { data } = props;
+
     return (
-        <div>
-            {`asd`}
+        <div className="container">
+            {data.map((item) => (
+                <div className="cardContainer" key={item.id}>
+                    <picture>
+                        <img className="cardImage" src={item.image} alt={item.title} />
+                    </picture>
+                    <h1 className="cardTitle line-clamp-1">
+                        {item.title}
+                    </h1>
+                    <p className="cardDescription line-clamp-2">
+                        {item.description}
+                    </p>
+                </div>
+            ))}
         </div>
     )
 }
